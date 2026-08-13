46e300433e9c2bf2e1ccbb2e61ca31a2a0f5e378
Qasim Ahmad <qasimahmad2555@gmail.com>
2026-08-13 21:58:43 +0500

    implemented a minimlistic chatbot

---
+++ 4.chatbot/chatbot.py
@@ -0,0 +1,18 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
+
+chat_history = [] # for storing the chat messages
+while True:
+    user_Input = input("You: ")
+    chat_history.append(user_Input)
+    if user_Input == "exit":
+        break
+    result = llm.invoke(chat_history)
+    chat_history.append(result.content[0]["text"])
+    print("AI: ", result.content[0]["text"])
+
+print(chat_history)
