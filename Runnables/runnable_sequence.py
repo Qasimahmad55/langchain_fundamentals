2d310bf7b2b54e071ea66ef4c53edb6248bad7d0
Qasim Ahmad <qasimahmad2555@gmail.com>
2026-09-01 16:26:21 +0500

    basics of runnableSequence

---
+++ Runnables/runnable_sequence.py
@@ -0,0 +1,19 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
+
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template="Write a joke about {topic}", input_variables=["topic"]
+)
+
+chain = RunnableSequence(prompt, model, parser)
+
+print(chain.invoke({"topic": "AI"}))
