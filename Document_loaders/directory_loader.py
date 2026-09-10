d1eb95099504e14d30a2b1b46235fe30da86e972
Qasim Ahmad <qasimahmad2555@gmail.com>
2026-09-10 14:48:46 +0500

    understand how to handle a large amount of documents

--- Document_loaders/directory_loader.py
+++ Document_loaders/directory_loader.py
@@ -2,6 +2,8 @@ from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 
 loader = DirectoryLoader(path="Document_loaders", glob="*.pdf", loader_cls=PyPDFLoader)
 
-docs = loader.load()
+# docs = loader.load() # used for instantly loading documents
+
+docs = loader.lazy_load()  # used for handling a large amount of documents
 
 print(docs[0].page_content)
